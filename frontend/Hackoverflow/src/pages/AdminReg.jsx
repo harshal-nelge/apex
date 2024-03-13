e4fdb4d87757d463c82792aa2c78c3e542ada6e7
HARSHB0810 <138614246+HARSHB0810@users.noreply.github.com>
2024-03-13 15:47:51 +0530

    Update AdminReg.jsx

--- frontend/Hackoverflow/src/pages/AdminReg.jsx
+++ frontend/Hackoverflow/src/pages/AdminReg.jsx
@@ -4,66 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const AdminReg = () => {
-<<<<<<< Updated upstream
-  const [name, setName] = useState("");
-  const [pass, setPass] = useState("");
-  const [admin, setAdmin] = useState("");
- 
-  const handleSubmit = async () => {
-    console.log("Handle submit is working");
-    try {
-      const response = await fetch(
-        "http://localhost:5000/auth/admin/register",
-        {
-          method: "post",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({
-            username: name,
-            password: pass,
-            adminName: admin,
-          }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const responseData = await response.json();
-      console.log(responseData);
-      setData(responseData);
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-    }
-  };
-
-  return (
-    <div className="flex  justify-center items-center h-screen w-full space-y-2 flex-col ">
-      <form>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={(e) => setName(e.target.value)}
-          className=" outline placeholder-black p-2"
-        />
-
-        <input
-          type="text"
-          placeholder="admin name"
-          onChange={(e) => setAdmin(e.target.value)}
-          className=" outline placeholder-black p-2"
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPass(e.target.value)}
-          className=" outline placeholder-black p-2 "
-        />
-        <button onClick={handleSubmit}> submit</button>
-   
-=======
     const [data,setData] = useState("")
     const userName = useRef()
     const adminName = useRef()
@@ -107,7 +47,6 @@ const AdminReg = () => {
       />
       <button type="submit" className="px-4 py-2  bg-slate-900 text-white rounded-lg hover:bg-white hover:text-slate-900">Submit</button>
       
->>>>>>> Stashed changes
       </form>
       </div>
 
