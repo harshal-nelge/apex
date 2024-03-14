2fc7c5215658f0673f159b3f233bd973886d7436
yash15_exe <yashraikar152@gmail.com>
2024-03-14 20:23:41 +0530

    changes

--- Server/Routes/departmentRoutes.js
+++ Server/Routes/departmentRoutes.js
@@ -3,6 +3,7 @@ import { validateDepartment } from "../Middlewares/validateDepartment.js";
 import {
   addWork,
   departmentDashboardController,
+  myWorks,
 } from "../Controllers/departmentControllers.js";
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.post(
 );
 
 router.post("/addWorks", validateDepartment, addWork);
+router.post("/myWorks", validateDepartment, myWorks);
 
 export default router;
